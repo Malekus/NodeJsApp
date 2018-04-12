4197a4a5430e9d9cb86fca50e4fdba26b193d8f7
ChefDeProjet <QueDesPiratesIci@piratus.net>
2018-04-12 21:57:21 +0200

    en cours

--- serveurResize.js
+++ serveurResize.js
@@ -1,41 +1,58 @@
+var app = require('express');
 var http = require('http');
-var jimp = require('jimp')
+var jimp = require('jimp');
+const fs = require('fs');
 
+
+app.get('/image', function(req, res){
+	res.sendFile(__dirname + '/image');
+  });
+
+  http.Server(app).listen(1234);
+
+  /*
 httpServer = http.createServer(function(req, res){
   console.log("Un mec s'est connecté");
   res.end("Salut toi");
 });
 
-httpServer.listen(1234);
+httpServer.listen(1234);*/
 
 var io = require('socket.io').listen(httpServer);
 
 // http://localhost:1234/socket.io/socket.io.js
 io.sockets.on('connection', function(socket){
-	console.log("Nouveau mec");
-
 	socket.on('resize', function(params){
-		console.log(params);
-		var url = {
-				home: 'home',
-				jouer:'jouer',
-				option:'option',
-				ladder:'ladder'
-			};
+		var url = {};
+		fs.readdir("image/personne", function(err, items){
+			items.forEach(function(element, index){
+				jimp.read("image/personne/"+ element, function(err, image){
+					image.resize(params.height, params.width)
+						.quality(90)
+						.write("resize/personne/"+params.height+"x"+params.width+""+element);
+				});
+				console.log(''+element.replace(".png",""));
+				console.log("http://localhost:1234/resize/personne/"+params.height+"x"+params.width+""+element);
+				url[''+element.replace(".png","")] = "http://localhost:1234/resize/personne/"+params.height+"x"+params.width+""+element;
+			});
+		});
 
-			/*jimp.read("lenna.png", function (err, image) {
-    		if (err) throw err;
-    		lenna.resize(256, 256)            // resize 
-		         .quality(90)                // set greyscale 
-		         .write("lena-small-bw.jpg"); // save 
-			});*/
+		console.log(url);
 
-			jimp.read("image/personne/F_blond_marche.png", function(err, image){
-				image.resize(params.height, params.width)
-					.quality(90)
-					.write("resize/personne/F_blond_marcheR.png");
-			})
-		
 		socket.emit('url', url);
-	})
+	});
 })
+	
+
+	
+
+
+		
+		
+
+	
+
+
+
+		
+	
